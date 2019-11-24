45c440f41cbd7c887fc5e809cf684e52a7c3c963
Chirranjit Ghosh <50874268+CHIRRANJIT@users.noreply.github.com>
2019-11-24 20:13:56 +0530

    Add files via upload

---
+++ repeat.py
@@ -0,0 +1,15 @@
+# Python3 program to find the only 
+# repeating element in an array where 
+# elements are from 1 to n-1. 
+
+def findRepeating(arr, n): 
+	
+	# Find array sum and subtract sum 
+	# first n-1 natural numbers from it 
+	# to find the result. 
+	return sum(arr) -(((n - 1) * n) // 2) 
+
+# Driver Code 
+arr = [9, 8, 2, 6, 1, 8, 5, 3, 4, 7] 
+n = len(arr) 
+print(findRepeating(arr, n)) 
