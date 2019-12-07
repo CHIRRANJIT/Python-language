cb87f76d4b3b600e9194ac3a179a4c7fe90512d0
Chirranjit Ghosh <50874268+CHIRRANJIT@users.noreply.github.com>
2019-12-07 21:45:01 +0530

    Add files via upload
    Introspection is an ability to determine the type of an object at runtime. Everything in python is an object. Every object in Python may have attributes and methods. By using introspection, we can dynamically examine python objects. Code Introspection is used for examining the classes, methods, objects, modules, keywords and get information about them so that we can utilize it. Introspection reveals useful information about your program’s objects. Python, being a dynamic, object-oriented programming language, provides tremendous introspection support. Python’s support for introspection runs deep and wide throughout the language.

---
+++ code-introspection.py
@@ -0,0 +1,18 @@
+# Python program showing 
+# a use of str() function 
+
+a = 1
+print(type(a)) 
+
+# converting integer 
+# into string 
+a = str(a) 
+print(type(a)) 
+
+s =[1, 2, 3, 4, 5] 
+print(type(s)) 
+
+# converting list 
+# into string 
+s = str(s) 
+print(type(s)) 
