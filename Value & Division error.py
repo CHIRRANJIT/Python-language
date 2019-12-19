5b286df0cab42d3fe56b5482c408df76683f8e9a
Chirranjit Ghosh <50874268+CHIRRANJIT@users.noreply.github.com>
2019-12-19 19:29:16 +0530

    Add files via upload

---
+++ Value & Division error.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the 1st no:"))
+b=int(input("Enter the 2nd no:"))
+result = a/b
+print("result:",result)
+    except value error:
+        print("Value error occurred")
+    except division error:
+        print("Division error occurred")
+    else:
+        print("Division sucessful")
